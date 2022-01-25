54bd62a8e85e484b368389b3588cc521ee8eafcc
Eric Greene <eric@t4d.io>
2022-01-25 18:07:51 -0500

    add solution to xhr exercise

--- ts-demo/src/scripts/app.ts
+++ ts-demo/src/scripts/app.ts
@@ -1,22 +1,40 @@
 
 // const delay = () => Math.floor(Math.random() * 1000) + 500;
 
-const xhr = new XMLHttpRequest();
+type Car = {
+  id: number;
+  make: string;
+  model: string;
+  year: number;
+  color: string;
+  price: number;
+};
 
-xhr.addEventListener('readystatechange', () => {
+function myFetch<T>(url: string) {
 
-  if (xhr.status === 200 && xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+  return new Promise<T>( resolve => {
 
-});
+    const xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://localhost:5050/cars');
-xhr.send();
+    xhr.addEventListener('readystatechange', () => {
+    
+      if (xhr.status === 200 && xhr.readyState === 4) {
+        resolve(JSON.parse(xhr.responseText));
+      }
+    
+    });
+    
+    xhr.open('GET', url);
+    xhr.send();
+
+  });
+
+}
+
 
 // Implement the `myFetch` function as shown below
 // use the XHR example to execute the actual AJAX
 
-myFetch('http://localhost:5050/cars').then(cars => console.log(cars));
+myFetch<Car[]>('http://localhost:5050/cars').then(cars => console.log(cars));
 
 export { }
